1608deff4a2db18f5bfacb0623c3ecd7341b18b1
Theo Weller <weller.theodor@gmail.com>
2019-06-25 15:39:55 -0400

    refactored routes to only allow access to the ones we need

--- Pokemon-Crisis-Backend/app/controllers/api/v1/trainers_controller.rb
+++ Pokemon-Crisis-Backend/app/controllers/api/v1/trainers_controller.rb
@@ -1,27 +1,21 @@
 class Api::V1::TrainersController < ApplicationController
-
   def index
     @trainers = Trainer.all
     @sorted_trainers = @trainers.sort_by {|a| -a.win.to_i}
-    
     render json: @sorted_trainers.first(5)
   end
-
 
   def create
     @player1 = Trainer.find_or_create_by(name: params["player1"])
     @player2 = Trainer.find_or_create_by(name: params["player2"])
-
     @players = {player1: @player1, player2: @player2}
     render json: @players
   end
 
   def update
-    puts params
     @winner = Trainer.find(params["winner"])
     @winner.update(win: @winner.win.to_i + 1)
     @loser = Trainer.find(params["loser"])
     @loser.update(loss: @loser.loss.to_i + 1)
   end
-
 end
--- Pokemon-Crisis-Backend/config/routes.rb
+++ Pokemon-Crisis-Backend/config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :trainers
-      resources :pokemons
+      post '/trainers', to: 'trainers#create'
+      post '/trainers/:id', to: 'trainers#update'
+      get '/pokemons', to: 'pokemons#index'
+      get '/trainers', to: 'trainers#index'
       resources :types, only: [:index, :show]
     end
   end
 end
+
+# get 'users/:id/photos/:photo_id', to: 'users#edit_photo'
